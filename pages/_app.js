1b256805bd046e909ea0cfad7c29ce1c676e5359
eunnho98 <taemin4u@khu.ac.kr>
2023-12-18 00:01:08 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  fonts: {
+    heading: `'Jua', sans-serif`,
+    body: `'Jua', sans-serif`,
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
